5706f1d7c9c0d8225a09ffd86da6dec74743faf9
Igors Krasjukovs <tahq69@gmail.com>
2017-08-22 21:45:15 +0300

    initial structure and elements;

---
+++ src/Graph.vue
@@ -0,0 +1,23 @@
+<template>
+  <canvas id="graph-canvas" width="800px" height="800px"></canvas>
+</template>
+
+<script>
+  export default {
+    name: 'graph-canvas',
+
+    data () {
+      return {
+        context: {}
+      }
+    },
+
+    mounted () {
+      this.context = this.$el.getContext('2d')
+      this.context.beginPath()
+      this.context.moveTo(0, 0)
+      this.context.lineTo(300, 150)
+      this.context.stroke()
+    }
+  }
+</script>
